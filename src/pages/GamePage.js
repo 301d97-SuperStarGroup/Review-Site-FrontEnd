f0b75bd9e4ac308b96b136de01071c8db384f51b
ReeceRenninger <renningerreece@gmail.com>
2023-04-03 15:15:09 -0700

    auth continued

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,34 +1,34 @@
 
-// import axios from 'axios';
+import axios from 'axios';
 import React from 'react';
 
 class GamePage extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      error: false,
+      errorMessage: '',
+      games: []
+    }
+  }
+  handleGameLoad = async (event) => {
+    try {
+      let url = `https://www.freetogame.com/api/games/games`;
+      let gameDataFromAxios = await axios.get(url);
+      console.log(gameDataFromAxios.data);
+      this.setState({
+        games: gameDataFromAxios.data,
+        error: false
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      });
+    }
+  }
   render() {
-    // constructor(props){
-    //   super(props);
-    //   this.state = {
-    //     error: false,
-    //     errorMessage: '',
-    //     games: []
-    //   }
-    // }
 
-    // handleGameLoad = async (event) => {
-    //   try {
-    //     let url = `${process.env.REACT_APP_SERVER}/games`;
-    //     let gameDataFromAxios = await axios.get(url);
-
-    //     this.setState({
-    //       games: gameDataFromAxios.data,
-    //       error: false
-    //     });
-    //   } catch (error) {
-    //     this.setState({
-    //       error: true,
-    //       errorMessage: error.message
-    //     });
-    //   }
-    // }
 
     // componentDidMount(){
     //   this.handleGameLoad();
