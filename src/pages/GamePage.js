83d5da94cd15548fa7a8cd1f778d51a080be34eb
ReeceRenninger <renningerreece@gmail.com>
2023-04-05 14:06:19 -0700

    deleted extra button

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -186,9 +186,6 @@ class GamePage extends React.Component {
               </ListGroup>
 
               <button style={{display: 'block', margin: 'auto'}} onClick={()=>{ this.handleSaveGame(game)}} class="nes-btn is-success">SAVE</button>
-              
-
-              <Button style={{display: 'block', margin: 'auto'}} onClick={()=>{ this.handleSaveGame(game)}} variant="success">SAVE</Button>
 
 
             </Card.Body>
